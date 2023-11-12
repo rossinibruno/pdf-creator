891a281ff7a3360bec23ae7e94ce2493ee8c2abf
Bruno Erick Rossini <bruno.rossini@BAPBR-C02GV02Z0KPF.local>
2023-11-12 15:43:00 -0300

    feat: upload file

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ autentiqueQueue.process(async function (job, done) {
 
   console.log(attributes);
 
-  autentique.document.create(attributes);
+  await autentique.document.create(attributes);
 
   return done(null, job.data);
 });
