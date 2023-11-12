519c614f33fba5856a549f81f32554dc473c0847
Bruno Erick Rossini <bruno.rossini@BAPBR-C02GV02Z0KPF.local>
2023-11-12 14:52:47 -0300

    feat: upload file

--- index.js
+++ index.js
@@ -30,40 +30,9 @@ app.use(express.json());
 app.post("/", async function (req, res) {
   const { name } = req.body;
 
-  // const job = queue.createJob({ name });
+  const job = queue.createJob({ name });
 
-  // await job.save();
-
-  const attributes = {
-    document: {
-      name: "NOME DO DOCUMENTO",
-    },
-    signers: [
-      {
-        email: "email@email.com",
-        action: "SIGN",
-        positions: [
-          {
-            x: "50", // Posição do Eixo X da ASSINATURA (0 a 100)
-            y: "80", // Posição do Eixo Y da ASSINATURA (0 a 100)
-            z: "1", // Página da ASSINATURA
-          },
-          {
-            x: "50", // Posição do Eixo X da ASSINATURA (0 a 100)
-            y: "50", // Posição do Eixo Y da ASSINATURA (0 a 100)
-            z: "2", // Página da ASSINATURA
-          },
-        ],
-      },
-      {
-        email: "email@email.com",
-        action: "SIGN",
-      },
-    ],
-    file: "https://jucisrs.rs.gov.br/upload/arquivos/201710/30150625-criacao-de-pdf-a.pdf",
-  };
-
-  console.log(JSON.stringify(autentique));
+  await job.save();
 
   res.send("Adicionado a fila para criação de documento");
 });
