2683f41119d889cb32a03a58f11b798e0ebd4f9b
Bruno Erick Rossini <bruno.rossini@BAPBR-C02GV02Z0KPF.local>
2023-11-12 15:16:09 -0300

    feat: upload file

--- htmltopdf.js
+++ htmltopdf.js
@@ -1,8 +1,6 @@
 const { html } = require("@prairielearn/html");
 const { renderEjs } = require("@prairielearn/html-ejs");
 const fs = require("fs");
-// const supabase = require("./supabase");
-// const autentique = require("autentique-v2");
 
 const upload = require("./supabase");
 
@@ -18,22 +16,16 @@ module.exports = createPdf = async (params, filename) => {
   const html = renderHtml(params);
 
   return new Promise(function (resolve, reject) {
-    // const stream = fs.createWriteStream(`${filename}.pdf`);
-    wkhtmltopdf(html).pipe(
-      console.log
-      // supabase.storage.from("documents").upload(`${filename}.pdf`, stream)
-    );
+    const stream = fs.createWriteStream(`./pdf/${filename}.pdf`);
+    wkhtmltopdf(html).pipe(stream);
 
-    // stream.on("finish", async () => {
-    //   const src = fs.createReadStream(`./${filename}.pdf`);
+    stream.on("finish", async () => {
+      console.log(html);
 
-    //   await supabase.storage.from("documents").upload(`${filename}.pdf`, src);
+      console.log(`Save document ${filename}.pdf`);
 
-    //   // await upload(`${filename}.pdf`, src);
-    //   console.log(html);
-
-    //   resolve();
-    // });
-    // stream.on("error", reject); // or something like that. might need to close `hash`
+      resolve();
+    });
+    stream.on("error", reject); // or something like that. might need to close `hash`
   });
 };
