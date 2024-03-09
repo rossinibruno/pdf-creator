a1798ce0c14668d40fd8905cca5fb8bd15ca24fa
Bruno Erick Rossini <bruno.rossini@BAPBR-C02GV02Z0KPF.local>
2024-03-09 19:04:42 -0300

    feat: negotiation update

--- supabase.js
+++ supabase.js
@@ -2,7 +2,9 @@ const { createClient } = require("@supabase/supabase-js");
 
 const { SUPABASE_PROJECT, SUPABASE_TOKEN } = process.env;
 
-module.exports = supabase = createClient(
+const supabase = createClient(
   `https://${SUPABASE_PROJECT}.supabase.co`,
   `${SUPABASE_TOKEN}`
 );
+
+module.exports = supabase;
