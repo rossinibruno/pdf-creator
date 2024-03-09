9431e66d2954d680ac2b293a5ce8c1c4428f6231
Bruno Erick Rossini <bruno.rossini@BAPBR-C02GV02Z0KPF.local>
2024-03-09 18:47:04 -0300

    feat: negotiation update contractId

--- supabase.js
+++ supabase.js
@@ -6,34 +6,3 @@ module.exports = supabase = createClient(
   `https://${SUPABASE_PROJECT}.supabase.co`,
   `${SUPABASE_TOKEN}`
 );
-
-// const fetch = require("node-fetch");
-// const FormData = require("form-data");
-
-// const API_URL = process.env.SUPABASE_URL;
-// const AUTH_TOKEN = process.env.SUPABASE_TOKEN;
-
-// /**
-//  * @async
-//  * @param {string} path
-//  * @param {ReadableStream} stream
-//  * @returns {void}
-//  */
-// module.exports = async function upload(path, stream) {
-//   console.log(stream);
-
-//   const form = new FormData();
-//   form.append("", stream);
-
-//   const result = await fetch(`${API_URL}/storage/v1/object/documents/${path}`, {
-//     method: "POST",
-//     body: form,
-//     headers: {
-//       Authorization: `Bearer ${AUTH_TOKEN}`,
-//       apikey: AUTH_TOKEN,
-//     },
-//   });
-
-//   console.log(result);
-//   return result;
-// };
